2de8a1070c94c2f9faf8dec204710ef59e5981f6
devdavix2 <devdavixx@gmail.com>
2024-11-10 19:51:32 +0100

    images changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: React.FC = () => {
                 className="text-transparent bg-clip-text saturate-150"
                 style={{
                   WebkitBoxDecorationBreak: 'clone',
-                  backgroundImage: "url('/static/aurora-blur.jpg')",
+                  backgroundImage: "url('/images/aurora-blur.jpg')",
                   backgroundSize: '200% auto',
                   backgroundPosition: '50% 40%',
                   backgroundColor: '#000',
